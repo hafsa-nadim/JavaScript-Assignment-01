e32de38c1c712f0dbb3ffe6ccf02ed3657ac8865
Sheikh Hafsa Nadeem <124384550+hafsa-nadim@users.noreply.github.com>
2023-04-04 08:41:36 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//Qn.01 Write a script to greet your website visitor using JS alert box.
+
+//alert("Welcome to our Website");
+
+//Qn.02 Write a script to display following message on your web page.
+
+//alert(" JavaScript Alert\n\n Error! Please enter a valid password.");
+
+//Qn.03. Write a script to display following message on your web page (Hint : Use line break)
+
+//alert(" JavaScript Alert \n\n Welcome to JS Land...\n Happy Coding!");
+
+//Qno.04 Write a script to display following messages in sequence.
+
+//alert(" JavaScript Alert\n\n Welcome to JS Land...");
+//alert(" JavaScript Alert\n\n Happy Coding");
+
+//Qno.05 Generate the following message through browser’s developer console.
+
+alert(" JavaScript Alert\n\n Hello... I can run JS through my web browser's console");
+
